b886f3f0f3a3d0bfa1b8819bb4ee419a08452a8d
AL0217 <101471112+AL0217@users.noreply.github.com>
2023-02-22 18:35:23 +0000

    Add files via upload

--- NetworkApplications.py
+++ NetworkApplications.py
@@ -11,6 +11,7 @@ import random
 import traceback # useful for exception handling
 import threading
 import select
+import io
 
 def setupArgumentParser() -> argparse.Namespace:
         parser = argparse.ArgumentParser(
@@ -122,7 +123,9 @@ class ICMPPing(NetworkApplication):
 
     def receiveOnePing(self, icmpSocket, destinationAddress, ID, timeSent, timeout):
         # 1. Wait for the socket to receive a reply
+        global time_received
         icmpSocket.settimeout(timeout)
+        time_received = 0
         timeleft = timeout
         # 2. Once received, record time of receipt, otherwise, handle a timeout
         while True:
@@ -231,11 +234,9 @@ class Traceroute(NetworkApplication):
         # 1. Build ICMP header
         ECHO_REQUEST = 8
         header = struct.pack("!BBHHH", ECHO_REQUEST, 0, 0, ID, 1)
-        data = []
-        data = bytes(data)
 
         # 2. Checksum ICMP packet using given function
-        checked = self.checksum(header + data)
+        checked = self.checksum(header)
         # 3. Insert checksum into packet    
         packet = struct.pack("!BBHHH", ECHO_REQUEST, 0, checked, ID, 1)
         # print('size ' ,sys.getsizeof(data))
@@ -254,7 +255,7 @@ class Traceroute(NetworkApplication):
         icmpSocket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
     
         # 2. Call sendOnePing function
-        timeSent = self.sendOnePing(icmpSocket, destinationAddress, 1)      #id is 1
+        timeSent = self.sendOnePing(icmpSocket, destinationAddress, 0x4321)      #id is 1
         # 3. Call receiveOnePing function
         flag, delay = self.receiveOnePing(icmpSocket, destinationAddress, 1, timeSent, timeout)
         # 4. Close ICMP socket
@@ -282,10 +283,9 @@ class Traceroute(NetworkApplication):
 class ParisTraceroute(NetworkApplication):
     def receiveOnePing(self, icmpSocket, timeSent, timeout):
         global timeleft, time_received, data
-        # 1. Wait for the socket to receive a reply
         icmpSocket.settimeout(timeout)
         timeleft = timeout
-        # 2. Once received, record time of receipt, otherwise, handle a timeout
+
         while True:
             try:
                 data = None
@@ -294,39 +294,80 @@ class ParisTraceroute(NetworkApplication):
                 if  data != None:
                     break
             except socket.timeout:
-                self.printAdditionalDetails()
                 return None, 0
-        # 3. Compare the time of receipt to time of sending, producing the total network delay
-        # print("time received: ", time_received)
         delay =  time_received - timeSent
-        # 4. Unpack the packet header for useful information, including the ID
+        
         icmp_header = data[20:28]
-        type, code, checksum, packet_id, sequence = struct.unpack('!BBHHH', icmp_header)
-        # print(int(data[0:20], 16))
+        type, code, checksum, packet_id, sequence = struct.unpack('BBHHH', icmp_header)
 
-        # 6. Return total network delay
+        return addr[0], delay * 1000
+
+    def udp_receiveOnePing(self, udpSocket, timeSent, timeout):
+        global timeleft, time_received, data
+        udpSocket.settimeout(timeout)
+        timeleft = timeout
+
+        while True:
+            try:
+                data = None
+                data, addr = udpSocket.recvfrom(1024)
+                time_received = time.time()
+                if  data != None:
+                    print(data)
+                    break
+            except socket.timeout:
+                return None, 0
+        delay =  time_received - timeSent
+        
+        # icmp_header = data[20:28]
+        # type, code, checksum, packet_id, sequence = struct.unpack('BBHHH', icmp_header)
+
         return addr[0], delay * 1000
 
     def sendOnePing(self, icmpSocket, destinationAddress, ID):
         # 1. Build ICMP header
         ECHO_REQUEST = 8
-        header = struct.pack("!BBHHH", ECHO_REQUEST, 0, 0, ID, 1)
+        header = struct.pack("BBHHH", ECHO_REQUEST, 0, 0, ID, 1)
         data = []
         data = bytes(data)
 
         # 2. Checksum ICMP packet using given function
         checked = self.checksum(header + data)
         # 3. Insert checksum into packet    
-        packet = struct.pack("!BBHHH", ECHO_REQUEST, 0, checked, ID, 1)
+        packet = struct.pack("BBHHH", ECHO_REQUEST, 0, checked, ID, 1)
         # print('size ' ,sys.getsizeof(data))
 
         # 4. Send packet using socket
-        while packet:
-            sent = icmpSocket.sendto(packet, (destinationAddress, 1))
-            packet = packet[sent:]
+        sent = icmpSocket.sendto(packet+data, (destinationAddress, 1))
+
         # 5. Record time of sending
         timeSent = time.time()
-        return timeSent
+        return timeSent, sent
+        pass
+
+    def udp_sendOnePing(self, udpSocket, destinationAddress):
+        # 1. Build UDP header
+        LOCAL_PORT = 25565
+        DEST_PORT = 25566
+        str = "Fuck you"
+
+        udpSocket.bind(("127.0.0.1", LOCAL_PORT))
+        data = []
+        data = bytes(data)
+        header = struct.pack("HHHH", LOCAL_PORT, DEST_PORT, 8+sys.getsizeof(data), 0)
+
+        # 2. Checksum ICMP packet using given function
+        checked = self.checksum(header + data)
+        print(checked)
+        # 3. Insert checksum into packet
+        header = struct.pack("HHHH", LOCAL_PORT, DEST_PORT, 8+sys.getsizeof(data), checked)
+
+        # 4. Send packet using socket
+        sent = udpSocket.sendto(header + data, (destinationAddress, 12345))
+        print('sent!!')
+        # 5. Record time of sending
+        timeSent = time.time()
+        return timeSent, sent
         pass
 
     def tracing(self, destinationAddress, timeout, ttl):
@@ -334,14 +375,30 @@ class ParisTraceroute(NetworkApplication):
         icmpSocket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
     
         # 2. Call sendOnePing function
-        timeSent = self.sendOnePing(icmpSocket, destinationAddress, 1)      #id is 1
+        timeSent, byte = self.sendOnePing(icmpSocket, destinationAddress, 1)      #id is 1
         # 3. Call receiveOnePing function
         src_ip, delay = self.receiveOnePing(icmpSocket, timeSent, timeout)
         # 4. Close ICMP socket
         icmpSocket.close()
         # 5. Return total network delay
-        return src_ip, delay
+        return src_ip, delay, byte
         pass
+
+    def udp_tracing(self, destinationAddress, timeout, ttl):
+        udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udpSocket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+    
+        # 2. Call sendOnePing function
+        timeSent, byte = self.udp_sendOnePing(udpSocket, destinationAddress)      #id is 1
+        # 3. Call receiveOnePing function
+        src_ip, delay = self.udp_receiveOnePing(udpSocket, timeSent, timeout)
+        # 4. Close ICMP socket
+        udpSocket.close()
+        # 5. Return total network delay
+        return src_ip, delay, byte
+        pass
+
+
 
 
     def __init__(self, args):
@@ -349,49 +406,91 @@ class ParisTraceroute(NetworkApplication):
         print('Paris-Traceroute to: %s...' % (args.hostname))
         ip = socket.gethostbyname(args.hostname)
         ttl = 1
-        ipArr = []
+        loss = 0
         while True:
-            time.sleep(1)
-            src_ip, ping = self.tracing(ip, 5, ttl)
-            if src_ip == None:              # If there's a timeout error
+                # time.sleep(1)
+                count = 0
+                delayList = []
+                for i in range(3):
+                    if args.protocol == 'icmp':
+                        src_ip, ping, byte = self.tracing(ip, args.timeout, ttl)
+                    elif args.protocol == 'udp':
+                        src_ip, ping, byte = self.udp_tracing(ip, args.timeout, ttl)
+                    delayList.append(ping)
+                    if src_ip == None:              # If there's a timeout error
+                        loss += 1
+                        self.printAdditionalDetails((loss/(ttl*3)) * 100)
+                        count += 1
+            
+                if count == 3:
+                    ttl += 1
+                    continue
+
+                try:
+                    name = socket.gethostbyaddr(src_ip)[0]
+                except:
+                    name = ""
+                self.printMultipleResults(ttl, ip, delayList, name)
                 ttl += 1
-                continue
-            try:
-                name = socket.gethostbyaddr(src_ip)[0]
-            except:
-                name = ""
-                pass
-            self.printOneResult(src_ip, 50, ping, ttl, name)
-            ttl += 1
-            if(src_ip not in ipArr):
-                ipArr.append(src_ip)
-                # print(src_ip)
-                # print(ipArr)
-            else:
-                ipArr.remove(src_ip)
-            if(src_ip == ip):
-                print(ipArr)
-                break
+                # if(src_ip not in ipArr):
+                #     ipArr.append(src_ip)
+                # else:
+                #     ipArr.remove(src_ip)
+                if(src_ip == ip):
+                    # print(ipArr)
+                    break
+            
+
+
+
+
+       
+
 
 class WebServer(NetworkApplication):
 
-    def handleRequest(tcpSocket):
+    def handleRequest(self, tcpSocket):
         # 1. Receive request message from the client on connection socket
+        data, src_ip = tcpSocket.recvfrom(1024)
         # 2. Extract the path of the requested object from the message (second part of the HTTP header)
+        url = data.split(b' ')[1].decode('utf-8')
+        url = url[1:]
         # 3. Read the corresponding file from disk
-        # 4. Store in temporary buffer
+        # try:
+        #     f = open(url, "rb")
+        #     response = "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n".format(len(buffer))
+        #     buffer = f.read()
+        #     tcpSocket.sendall(response.encode('utf-8') + buffer)
+        # except Exception:
+        #     print(Exception)
+        f = open(url, "rb")
+        buffer = f.read()
+        response = "HTTP/1.1 200 OK\r\n"
+        tcpSocket.sendall(response.encode('utf-8') + buffer)
         # 5. Send the correct HTTP response error
         # 6. Send the content of the file to the socket
         # 7. Close the connection socket
+        tcpSocket.close()
+        # Content-Length: {}\r\n\r\n         .format(len(buffer)
         pass
 
     def __init__(self, args):
         print('Web Server starting on port: %i...' % (args.port))
         # 1. Create server socket
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # 2. Bind the server socket to server address and server port
+        serverSocket.bind(("127.0.0.1", args.port))
         # 3. Continuously listen for connections to server socket
+        conn = None
+        while True:
+            serverSocket.listen(1)
+            conn, src_ip = serverSocket.accept()
+            print(type(conn))
         # 4. When a connection is accepted, call handleRequest function, passing new connection socket (see https://docs.python.org/3/library/socket.html#socket.socket.accept)
+            if conn != None:
+                self.handleRequest(conn)
         # 5. Close server socket
+            serverSocket.close()
 
 
 class Proxy(NetworkApplication):
